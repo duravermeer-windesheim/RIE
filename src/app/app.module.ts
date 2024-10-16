375e98555f164e129181c18dbc5c68ab6aedf42e
DustinJoosen <dustinjoosen2003@gmail.com>
2024-10-16 16:55:09 +0200

    Create shared entry component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {EntryComponent} from './shared/components/entry/entry.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		DashboardComponent,
+		EntryComponent
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/entry.model.ts
@@ -0,0 +1,10 @@
+
+export interface Entry {
+	label: string,
+	unit: string,
+	required?: boolean,
+	defaultValue?: number,
+	min?: number,
+	max?: number,
+	placeholder?: string
+}
