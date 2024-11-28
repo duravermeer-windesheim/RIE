5fb98cc429f3abe0509da133f51e898d38c8205b
Dustin Joosen <67952109+DustinJoosen@users.noreply.github.com>
2024-11-28 11:59:03 +0100

    Calculation details overview (#26)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {ColorService} from './services/color.service';
 import {FontService} from './services/font.service';
 import {fromEvent, mapTo, merge, Observable, of} from 'rxjs';
+import {MatDialog} from '@angular/material/dialog';
+import {SettingsDialogComponent} from './components/settings-dialog/settings-dialog.component';
+import {
+	CalculationOverviewDialogComponent
+} from './components/calculation-overview-dialog/calculation-overview-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -24,5 +29,4 @@ export class AppComponent {
 				"verbinding kunnen maken met de databank, en de risico's en maatrelen zullen niet gevuld worden.")
 		}
 	}
-
 }
